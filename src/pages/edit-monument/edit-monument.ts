647df1291c7d4a26a9580391010edbbc817532ea
JEricaM <catlady03@libero.it>
2017-10-23 14:02:12 +0200

    modificata upload-photo, messo una classe globale del css e altri fix

--- src/pages/edit-monument/edit-monument.ts
+++ src/pages/edit-monument/edit-monument.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { WelcomepagePage } from "../welcomepage/welcomepage";
 
 /**
  * Generated class for the EditMonumentPage page.
--- src/pages/upload-photo/upload-photo.ts
+++ src/pages/upload-photo/upload-photo.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { WelcomepagePage } from "../welcomepage/welcomepage"
+import { WelcomepagePage } from "../welcomepage/welcomepage";
 
 /**
  * Generated class for the UploadPhotoPage page.
