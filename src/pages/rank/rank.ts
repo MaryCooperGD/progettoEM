a8309a26b74ee065ccb88a55f06612c5c7526d3a
JEricaM <catlady03@libero.it>
2017-11-19 12:18:14 +0100

    bozza classifica

--- src/pages/rank/rank.ts
+++ src/pages/rank/rank.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Api } from "../../providers/api";
+import * as firebase from 'firebase/app';
 
 /**
  * Generated class for the RankPage page.
@@ -18,15 +19,32 @@ export class RankPage {
 
   username:any;
 
+  //Per la Classifica (rank.ts)
+  public items: Array<any> = [];
+  public itemRef: firebase.database.Reference = firebase.database().ref('/users');
+  constructor1(){}
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public api:Api) {
   }
 
   ionViewDidLoad() {
     if(this.api.user.displayName==null){
       this.username = '';
-  }else {
+    }else {
       this.username = this.api.user.displayName
   }    
+
+  //--- per la classifica
+  this.itemRef.on('value', itemSnapshot => {
+    this.items = [];
+    itemSnapshot.forEach( itemSnap => {
+      this.items.push(itemSnap.val());
+      return false;
+    });
+  });
+  //---fine classifica
+
+
   }
 
 }
