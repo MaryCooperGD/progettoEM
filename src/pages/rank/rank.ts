4970d7ef096f8d267e631b3661b7da7b2046e53d
Mary <marycoopergd@gmail.com>
2017-11-23 21:53:08 +0100

    fixed ranking

--- src/pages/rank/rank.ts
+++ src/pages/rank/rank.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Api } from "../../providers/api";
+import { UserService } from "../../providers/user_service";
 import * as firebase from 'firebase/app';
 
 
@@ -24,22 +25,22 @@ import * as firebase from 'firebase/app';
 export class RankPage {
 
   username:any;
-  //position;
+  position;
 
 
   //Per la Classifica (rank.ts)
   public items: Array<any> = [];
-  public itemRef: firebase.database.Reference = firebase.database().ref('/users');
+  public itemRef: firebase.database.Reference = firebase.database().ref('/users/');
   
   //---------CODICE DEL TIZIO-----------------//
-  /*constructor(public userService:UserService,public api: Api){
+  constructor(public userService:UserService,public api: Api){
     this.username = this.api.user.displayName;
-    this.userService.getUsers()//.getUserPosition(this.usern;
-    this.position = this.userService.getUserPosition(this.username); // you need to pass the user accessing the rank page
- }*/
+    //this.userService.getUsers()//.getUserPosition(this.usern;
+    //this.position = this.userService.getUserPosition(this.username); // you need to pass the user accessing the rank page
+ }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public api:Api) {
-  }
+  /*constructor(public navCtrl: NavController, public navParams: NavParams, public api:Api) {
+  }*/
 
   ionViewDidLoad() {
     if(this.api.user.displayName==null){
@@ -56,12 +57,34 @@ export class RankPage {
         this.items.push(itemSnap.val());
         return false;
       });
-      return this.items.reverse(); //Siccome da firebase i dati si estraggono solamente in ordine crescente, il reverse serve per ottenere l'ordinamento decrescente
+      var j = 0;
+      this.items.reverse().forEach(i=>{
+        j++;
+        if (new String(i.username).valueOf() == new String(this.username).valueOf()){
+          console.log("Trovato")
+          this.position = j;
+        }
+      })
+
+      return this.items; //Siccome da firebase i dati si estraggono solamente in ordine crescente, il reverse serve per ottenere l'ordinamento decrescente
     });
+    
   //---fine classifica
 
 
   } //Fine metodo ionViewDidLoad
+
+
+  calculatePosition(){
+    var j = 0;
+    this.items.forEach(i=>{
+      console.log("Username: " + i.username)
+      j++;
+      if (new String(i.username).valueOf() == new String(this.username).valueOf()){
+        this.position = j;
+      }
+    })
+  }
 }
 
 
