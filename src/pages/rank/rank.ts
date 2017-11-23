ba15dbc86ff8cda605a7c87dd2305b63b9787c6d
JEricaM <catlady03@libero.it>
2017-11-23 20:04:15 +0100

    fix pagina classifica

--- src/pages/rank/rank.ts
+++ src/pages/rank/rank.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Api } from "../../providers/api";
 import * as firebase from 'firebase/app';
-import {UserService} from '../../providers/user_service';
 
 
+// import {UserService} from '../../providers/user_service'; IMPORT DEL FILE DEL TIZIO
 
 /**
  * Generated class for the RankPage page.
@@ -24,67 +24,45 @@ import {UserService} from '../../providers/user_service';
 export class RankPage {
 
   username:any;
-  position;
+  //position;
 
 
   //Per la Classifica (rank.ts)
-  //public items: Array<any> = [];
-  //public itemRef: firebase.database.Reference = firebase.database().ref('/users');
+  public items: Array<any> = [];
+  public itemRef: firebase.database.Reference = firebase.database().ref('/users');
   
-  //Per la posizione giocatore
-  //public position: Array<any> = [];
-  //public positionRef: firebase.database.Reference = firebase.database().ref('/users');
-  constructor(public userService:UserService,public api: Api){
+  //---------CODICE DEL TIZIO-----------------//
+  /*constructor(public userService:UserService,public api: Api){
     this.username = this.api.user.displayName;
     this.userService.getUsers()//.getUserPosition(this.usern;
     this.position = this.userService.getUserPosition(this.username); // you need to pass the user accessing the rank page
- }
+ }*/
 
-
-
- // constructor(public navCtrl: NavController, public navParams: NavParams, public api:Api) {
- // }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api:Api) {
+  }
 
   ionViewDidLoad() {
-   /* if(this.api.user.displayName==null){
+    if(this.api.user.displayName==null){
       this.username = '';
     }else {
-      this.username = this.api.user.displayName*/
+      this.username = this.api.user.displayName
   }    
 
-  
-
    //--- per la classifica
-  //Ordina i dati in base al punteggio dei giocatori. Mostra la lista decrescente
-    /*this.itemRef.orderByChild("total_points").on('value',itemSnapshot =>{
-    this.items = [];
-    itemSnapshot.forEach( itemSnap => {
-      this.items.push(itemSnap.val());
-      return false;
+    //Ordina i dati in base al punteggio dei giocatori. Mostra la lista decrescente
+    this.itemRef.orderByChild("total_points").on('value',itemSnapshot =>{
+      this.items = [];
+      itemSnapshot.forEach( itemSnap => {
+        this.items.push(itemSnap.val());
+        return false;
+      });
+      return this.items.reverse(); //Siccome da firebase i dati si estraggono solamente in ordine crescente, il reverse serve per ottenere l'ordinamento decrescente
     });
-    return this.items.reverse(); //Siccome da firebase i dati si estraggono solamente in ordine crescente, il reverse serve per ottenere l'ordinamento decrescente
-  });*/
   //---fine classifica
 
 
-    //WRONG!!!!!!!!
-  //--per posizione giocatore.
-  //Praticamente fa una query sull'utente che corrisponde a quello loggato e restituisce 1 record. Grazie a quel record nell'html possiamo dare la posizione
- /* this.positionRef.orderByChild("username").equalTo(this.username).on('value',itemSnapshot =>{
-    this.position = [];
-    itemSnapshot.forEach( itemSnap => {
-      this.position.push(itemSnap.val());
-      return false;
-    });
-    return this.position;
-  });*/
-
- 
-  
-
-
-  }
-
+  } //Fine metodo ionViewDidLoad
+}
 
 
 
