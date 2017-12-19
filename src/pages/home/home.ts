4f625feb2e02044c1daf18f5dc4f8a86596b7e44
Mary <marycoopergd@gmail.com>
2017-12-19 18:10:18 +0100

    still working on duration/distance

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -100,7 +100,7 @@ export class HomePage {
       if (this.canCalculate){
 
         var startIcon = L.icon({
-          iconUrl: "../assets/images/start.png",
+          iconUrl: "assets/images/start.png",
 
           iconSize: [30,30],
           popupAnchor:  [1, -14],
@@ -108,7 +108,7 @@ export class HomePage {
         })
 
         var endIcon = L.icon({
-          iconUrl: "../assets/images/end.png",
+          iconUrl: "assets/images/end.png",
           
           iconSize: [30,30],
           popupAnchor:  [1, -14],
@@ -322,7 +322,6 @@ export class HomePage {
       myWayPts.push(data)
     })
 
-    myWayPts.shift();
 
     console.log("Lunghezza waypoints: " + myWayPts.length)
 
@@ -340,7 +339,6 @@ export class HomePage {
     }, (response, status) => {
       if (status === 'OK') {
 
-       // console.log("" + JSON.stringify(response))
         
         encoded = response.routes[0].overview_polyline; 
 
@@ -355,21 +353,26 @@ export class HomePage {
       });
       firstpolyline.addTo(newMap);
 
+
+      this.calculateMinimumLength(response, 300);
+
+
       console.log("Ordine " +response.routes[0].waypoint_order)
      var myRoute= response.routes[0].legs[0]
-     /* console.log("Prova mia" + JSON.stringify(myRoute.steps[0]))
-     console.log("Lunghezza: " + myRoute.steps.length)
-     for(var i=0; i<myRoute.steps.length; i++){ 
-       console.dir("I " + myRoute.steps[i].instructions)
-     } */
+    //console.dir("I " + myRoute.steps[i].instructions)
      var lunghezza = 0;
      var duration = 0;
+     var myLegs = response.routes[0].legs
      for(var i=0; i<myRoute.steps.length; i++){ 
        lunghezza+=myRoute.steps[i].distance.value
        duration+=myRoute.steps[i].duration.value
-      console.log("Step " + i + " lat long " + myRoute.steps[i].end_location.lat() + " " + myRoute.steps[i].end_location.lng())
     }
-    console.log("Numero di steps: " + myRoute.steps.length)
+    console.log("Numero di legs: " + myLegs.length)
+    for (var i = 0; i<myLegs.length; i++){
+      console.log("Leg " + i + " Start " + myLegs[i].start_address + " end " + myLegs[i].end_address)
+      
+    }
+
     console.log("Lunghezza percorso: " + lunghezza + " durata percorso: " + duration)
 
 
@@ -378,17 +381,14 @@ export class HomePage {
       waypoints.forEach(w=>{
         myArray.push(w)
       })
-      /* myArray.push([44.1359114,12.2454082,"Museo"])
-      myArray.push([44.1371501,12.24147,"Cattedrale"])
-      myArray.push([44.136342,12.2429801,"Ericacca"])
- */
+      
       var newArray =[]
 
       response.routes[0].waypoint_order.forEach(o=>{
         newArray.push(myArray[o])
       })
       let index = 1;
-      myArray.forEach(p =>{
+      newArray.forEach(p =>{
         let marker = L.marker([p.latlng[0],p.latlng[1]])
         let content = `<b>Nome</b>: ${p.nome}<br/>`+"Posizione " + index;
           marker.bindPopup(content) 
@@ -425,6 +425,136 @@ export class HomePage {
 
 
   }
+
+  calculateMinimumLength(response:any, maxLength){
+
+    var myLegs = response.routes[0].legs
+    var longest = 0;
+    var length = 0;
+    var items = [];
+    for (var i=0; i<myLegs.length; i++){
+      console.log("Leg "+ i + " length " + myLegs[i].distance.value)
+      items.push(myLegs[i].distance.value)
+      if(myLegs[i].distance.value > length){
+        longest = i;
+        length = myLegs[i].distance.value
+      }
+    }
+
+
+
+    this.subset(items, 10, maxLength)
+
+
+
+  }
+
+   
+  subset(people, min, max)
+  {
+    var subsets = [];
+    subsets[0] = '';
+  
+    for (var person in people)
+    {
+      for (var s = min-1; s >= 0; --s)
+      {
+        if (s in subsets)
+        {
+          var sum = s + person;
+  
+          if (!(sum in subsets))
+          {
+            subsets[sum] = subsets[s] + ' ' + person;
+  
+          }
+        }
+      }
+    }
+    
+    console.log("Prova " + subsets[sum])
+  }
+  
+
+  /* 0-1 knapsack problem
+For an overall introduction to knapsack problem, see https://en.wikipedia.org/wiki/Knapsack_problem
+Function name: knapsack
+Param: 
+  items: an array of {w: v:} (where 'w' stands for weight, and 'v' stands for value)
+  capacity: a positive integer number
+Will return max sum value that can reach, and the chosen subset to add up to the value.
+Example:
+var items = [{w:3,b:10},{w:1,b:3},{w:2,b:9},{w:2,b:5},{w:1,b:6}];
+var capacity = 6;
+console.log(knapsack(items, capacity));
+will return 
+{ maxValue: 25,
+  subset: [ { w: 1, v: 6 }, { w: 2, v: 9 }, { w: 3, v: 10 } ] }
+*/
+
+knapsack(items, capacity){
+  // This implementation uses dynamic programming.
+  // Variable 'memo' is a grid(2-dimentional array) to store optimal solution for sub-problems,
+  // which will be later used as the code execution goes on.
+  // This is called memoization in programming.
+  // The cell will store best solution objects for different capacities and selectable items.
+  var memo = [];
+
+  // Filling the sub-problem solutions grid.
+  for (var i = 0; i < items.length; i++) {
+    // Variable 'cap' is the capacity for sub-problems. In this example, 'cap' ranges from 1 to 6.
+    var row = [];
+    for (var cap = 1; cap <= capacity; cap++) {
+      row.push(getSolution(i,cap));
+    }
+    memo.push(row);
+  }
+
+  // The right-bottom-corner cell of the grid contains the final solution for the whole problem.
+  return(getLast());
+
+  function getLast(){
+    var lastRow = memo[memo.length - 1];
+    return lastRow[lastRow.length - 1];
+  }
+
+  function getSolution(row,cap){
+    const NO_SOLUTION = {maxValue:0, subset:[]};
+    // the column number starts from zero.
+    var col = cap - 1;
+    var lastItem = items[row];
+    // The remaining capacity for the sub-problem to solve.
+    var remaining = cap - lastItem.w;
+
+    // Refer to the last solution for this capacity,
+    // which is in the cell of the previous row with the same column
+    var lastSolution = row > 0 ? memo[row - 1][col] || NO_SOLUTION : NO_SOLUTION;
+    // Refer to the last solution for the remaining capacity,
+    // which is in the cell of the previous row with the corresponding column
+    var lastSubSolution = row > 0 ? memo[row - 1][remaining - 1] || NO_SOLUTION : NO_SOLUTION;
+
+    // If any one of the items weights greater than the 'cap', return the last solution
+    if(remaining < 0){
+      return lastSolution;
+    }
+
+    // Compare the current best solution for the sub-problem with a specific capacity
+    // to a new solution trial with the lastItem(new item) added
+    var lastValue = lastSolution.maxValue;
+    var lastSubValue = lastSubSolution.maxValue;
+
+    var newValue = lastSubValue + lastItem.v;
+    if(newValue >= lastValue){
+      // copy the subset of the last sub-problem solution
+      var _lastSubSet = lastSubSolution.subset.slice();
+      _lastSubSet.push(lastItem);
+      return {maxValue: newValue, subset:_lastSubSet};
+    }else{
+      console.log("SOlution " + lastSolution)
+      return lastSolution;
+    }
+  }
+}
 
   graphHopper(){
     
